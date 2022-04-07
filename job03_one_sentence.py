4f0db72e98fed1e744a7fe27f962a86c270010ef
hooah26 <hooah26@gmail.com>
2022-04-07 17:46:45 +0900

    1

--- job03_one_sentence.py
+++ job03_one_sentence.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('./crawling_data/reviews_20222.csv')
+df = pd.read_csv('./crawling_data/reviews_2021120.csv')
 print(df.head())
 print(df.duplicated().sum())
 df.drop_duplicates(inplace=True)
@@ -15,7 +15,7 @@ for title in df['title'].unique():
 df_one_sentences = pd.DataFrame(
     {'titles':df['title'].unique(), 'reviews':one_sentences})
 print(df_one_sentences.head())
-df_one_sentences.to_csv('./crawling_data/movie_review_onesentence_2018_20222.csv',
+df_one_sentences.to_csv('./crawling_data/movie_review_onesentence_reviews_2021120.csv',
                         index=False)
 
 
